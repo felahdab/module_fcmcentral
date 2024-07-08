9fbf65d14ac2fea2b05758dca4f38615a4a02384
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2024-07-08 21:56:34 +0200

    Ajout d'un menu et d'un lien pointant vers le panneau Filament.

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,9 @@
+
+            <div class="dropdown" >
+              <button class="btn btn-dark dropdown-toggle w-100" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              FCMCentral
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                @can('filament.fcmcentral::fcmcentral.pages.dashboard')<a class="dropdown-item" href="{{ route('filament.fcmcentral::fcmcentral.pages.dashboard')}}">Module FCM Central</a>@endcan
+              </div>
+            </div>
