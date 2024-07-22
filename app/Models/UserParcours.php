a7dac4a6df960a9eb4eac2f3101f4772872e25a6
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2024-07-22 22:33:43 +0200

    Conversion du modele UserParcours en modele commun.

--- app/Models/UserParcours.php
+++ app/Models/UserParcours.php
@@ -2,39 +2,9 @@
 
 namespace Modules\FcmCentral\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Modules\FcmCommun\Models\UserParcours as BaseModel;
 
-use Modules\FcmCentral\Traits\HasTablePrefix;
-
-use App\Models\User;
-use Modules\FcmCentral\Models\ParcoursSerialise;
-
-class UserParcours extends Model
+class UserParcours extends BaseModel
 {
-    use HasTablePrefix;
-    use HasUuids;
-
-    /**
-     * The attributes that are mass assignable.
-     */
-    protected $fillable = [ "user_id",
-                            "parcours_id",
-                            "parcours"];
-
-    protected $casts = [
-        'parcours' => 'array',
-    ];
-
-    public function user() 
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
-
-    public function parcoursserialise() 
-    {
-        return $this->belongsTo(ParcoursSerialise::class, 'parcours_id');
-    }
-
-    
+    protected $prefix = 'fcmcentral_';    
 }
