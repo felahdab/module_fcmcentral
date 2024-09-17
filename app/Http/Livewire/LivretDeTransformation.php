7cfb8739d9c491456714eb86725cd2293e5e1e9f
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2024-09-17 07:54:57 +0200

    wip

--- app/Http/Livewire/LivretDeTransformation.php
+++ app/Http/Livewire/LivretDeTransformation.php
@@ -4,8 +4,24 @@ namespace Modules\FcmCentral\Http\Livewire;
 
 use Modules\FcmCommun\Http\Livewire\Livret as BaseLivretDeTransformation;
 
+use Modules\FcmCommun\Services\LivretService;
+
+use Modules\FcmCentral\Services\ParcoursService;
+use Modules\FcmCentral\Models\MarinParcours;
+
 
 class LivretDeTransformation extends BaseLivretDeTransformation
 {
+    public function mount($uuid)
+    {
+        //dump('mounting');
+        $this->uuid = $uuid;
+        $this->livret = $this->livret_service->get_livret($this->uuid);;
+    }
 
+    public function boot()
+    {
+        //dump('boot');
+        $this->livret_service = new LivretService(new ParcoursService(), MarinParcours::class);
+    }
 }
