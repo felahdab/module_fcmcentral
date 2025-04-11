d0ea3e344dd669a4cb397314fe4a43dde979120c
Julien <julien.boisson@intradef.gouv.fr>
2025-04-11 09:26:52 +0000

    implements art #419581

---
+++ app/Filament/Fcmcentral/Pages/FcmMarinBis.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\FcmCentral\Filament\Fcmcentral\Pages;
+
+use Modules\FcmCommun\Filament\Fcmcommun\Resources\FcmMarinResource\Pages\ListFcmMarins;
+
+use Filament\Pages\Page;
+
+class FcmMarinBis extends ListFcmMarins
+{
+
+}
