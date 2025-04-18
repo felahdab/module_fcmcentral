21263785a4f48d1feee10da7e04d75344b1066e8
CV Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2025-04-18 08:31:04 +0000

    Ajustement du formulaire de l'action de la page de recherche dans l'annuaire.

--- app/Filament/Fcmcentral/Pages/RechercheAnnuaireForms/RechercheAnnuaireForm.php
+++ app/Filament/Fcmcentral/Pages/RechercheAnnuaireForms/RechercheAnnuaireForm.php
@@ -4,38 +4,35 @@ namespace Modules\FcmCentral\Filament\Fcmcentral\Pages\RechercheAnnuaireForms;
 
 use Illuminate\Support\Facades\DB;
 
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Toggle;
-use Filament\Forms\Components\Wizard;
+use Filament\Forms;
+use Filament\Forms\Get;
 
 use Modules\FcmCommun\Models\Cohorte;
 
 use Modules\FcmCentral\Models\ParcoursSerialise;
-use Modules\FcmCommun\Models\Marin;
+use Modules\FcmCentral\Models\Marin;
 
 class RechercheAnnuaireForm 
 {
     public static function getSchema()
     {
     return [
-        Wizard\Step::make('FCM')
-                ->hidden(true)
+        Forms\Components\Wizard\Step::make('FCM')
                 ->schema([
-                    Toggle::make('suivre_en_fcm')
-                        ->label("Suivre en FCM"),
-                    Select::make('cohorte')
+                    Forms\Components\Toggle::make('suivre_en_fcm')
+                        ->label("Suivre en FCM")
+                        ->live(),
+                    Forms\Components\Select::make('cohorte')
+                        ->visible(fn(Get $get) => $get('suivre_en_fcm'))
                         ->label("Cohorte de ce marin")
                         ->options(Cohorte::all()->pluck('libelle_long', 'id')),
-                    Select::make('mentor_id')
-                        //->options(Marin::pluck('nom','id'))
-                        // Recherche dans la base en concat
-                        ->options(Marin::select(
-                            'rh_marins.id',
-                            DB::raw("CONCAT(rh_marins.nom, ' ', rh_marins.prenom,' (', rh_grades.libelle_court, ')') as full_name"))
-                            ->leftjoin('rh_grades','rh_marins.grade_id','=','rh_grades.id')
-                            ->pluck('full_name','id'))
-                        ->searchable(),
-                    Select::make('parcoursserialise_id')
+                    Forms\Components\Select::make('mentor_id')
+                            ->label("Choisir un mentor")
+                            ->visible(fn(Get $get) => $get('suivre_en_fcm'))
+                            ->options(Marin::all()->pluck('fullNameAndGrade','id'))
+                            ->searchable(),
+                    Forms\Components\Select::make('parcoursserialise_id')
+                        ->visible(fn(Get $get) => $get('suivre_en_fcm'))
                         ->label('Choisir un Parcours')
                         ->options(ParcoursSerialise::pluck('libelle_court', 'id'))
                         ->searchable()
